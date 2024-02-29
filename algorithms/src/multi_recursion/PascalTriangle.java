6d676c56fb449850e566818dd4c58deb1cd783e2
sulaiman <sulaiman1015@gmail.com>
2024-02-29 23:41:11 +0100

    added files

--- algorithms/src/multi_recursion/PascalTriangle.java
+++ algorithms/src/multi_recursion/PascalTriangle.java
@@ -1,24 +1,63 @@
 package multi_recursion;
 
 public class PascalTriangle {
-
-    public static int triangle(int i, int j) {
+    //****************//
+    //two-dimensional array memoization
+    public static int triangle(int[][] element, int i, int j) {
+        if (element[i][j] > 0) {
+            return element[i][j];
+        }
         if (j == 0 || i == j) {
+            element[i][j] = 1;
             return 1;
         }
-        return triangle(i-1, j-1) + triangle(i-1, j);
+        element[i][j] = triangle(element,i-1, j-1) + triangle(element,i-1, j);
+        return element[i][j];
     }
 
     public static void printElement(int n) {
+        int[][] element = new int[n][];
         for (int i = 0; i < n; i++) {
+            element[i] = new int[i+1];
             printSpace(n, i);
             for (int j = 0; j <= i ; j++) {
-                System.out.printf("%-4d", triangle(i,j));
+                System.out.printf("%-4d", triangle(element, i, j));
             }
             System.out.println();
         }
     }
 
+
+
+
+    //****************//
+    //One-dimensional array memoization
+    public static void createRow(int[] row, int i) {
+        if(i == 0){
+            row[0] = 1;
+            return;
+        }
+        for (int j = i; j > 0; j--) {
+            row[j] = row[j] + row[j-1];
+        }
+    }
+
+    public static void printRow(int n) {
+        int[] row = new int[n];
+        for (int i = 0; i < n; i++) {
+            createRow(row,i);
+            printSpace(n, i);
+            for (int j = 0; j <= i ; j++) {
+                System.out.printf("%-4d", row[j]);
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+    //commons parties
     private static void printSpace(int n, int i) {
         int s = (n - 1 - i) * 2;
         for (int j = 0; j < s; j++) {
@@ -28,6 +67,7 @@ public class PascalTriangle {
 
     public static void main(String[] args) {
         //int v = triangle(4, 2);
-        printElement(5);
+        //printElement(5);
+        printRow(5);
     }
 }
