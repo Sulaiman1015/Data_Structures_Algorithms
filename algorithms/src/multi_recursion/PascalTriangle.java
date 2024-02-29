94adf6e465b3cf595b51ee8ba95e8a51714143c8
sulaiman <sulaiman1015@gmail.com>
2024-02-29 22:43:57 +0100

    added files

--- algorithms/src/multi_recursion/PascalTriangle.java
+++ algorithms/src/multi_recursion/PascalTriangle.java
@@ -3,24 +3,31 @@ package multi_recursion;
 public class PascalTriangle {
 
     public static int triangle(int i, int j) {
-        if (i == j || j == 0) {
+        if (j == 0 || i == j) {
             return 1;
         }
         return triangle(i-1, j-1) + triangle(i-1, j);
     }
 
-    public static void print(int n) {
+    public static void printElement(int n) {
         for (int i = 0; i < n; i++) {
-
+            printSpace(n, i);
             for (int j = 0; j <= i ; j++) {
-                System.out.print(triangle(i,j));
+                System.out.printf("%-4d", triangle(i,j));
             }
             System.out.println();
         }
     }
 
+    private static void printSpace(int n, int i) {
+        int s = (n - 1 - i) * 2;
+        for (int j = 0; j < s; j++) {
+            System.out.print(" ");
+        }
+    }
+
     public static void main(String[] args) {
         //int v = triangle(4, 2);
-        print(5);
+        printElement(5);
     }
 }
